e5e9800ef240e4438753241ce38750dfc5abecb8
erincunningham7 <101681089+erincunningham7@users.noreply.github.com>
2023-07-18 11:11:46 +0000

    Add constants to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "JvOFwS9_dc7TWA8ainp1RToMQVM";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementsById("resultsModal"));
